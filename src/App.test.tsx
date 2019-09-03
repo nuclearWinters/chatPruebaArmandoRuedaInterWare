fa169133cfb468e2d122d21dec073f5b03a3562e
nuclearWinters <armandonarcizoruedaperez@gmail.com>
2019-09-03 08:36:21 -0500

    resuelto error en test

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,10 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
-import renderer from 'react-test-renderer';
-import ChatMessages from './components/tabs/firstTab/chat/chatMessages/ChatMessages';
-import ChatHeader from './components/tabs/firstTab/chat/chatHeader/ChatHeader';
-import ChatInput from './components/tabs/firstTab/chat/chatInput/ChatInput';
 
 it('renders without crashing', () => {
   const div = document.createElement('div');
